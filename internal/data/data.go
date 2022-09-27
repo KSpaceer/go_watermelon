f77f11511799f0cc8fdb0c9bb1d1391e3b1e4527
Kirill <kirillsafin@bk.ru>
2022-09-28 00:13:45 +0300

    continue working

--- internal/data/data.go
+++ internal/data/data.go
@@ -9,12 +9,15 @@ import (
     "time"
     "strings"
 
+    _ "github.com/lib/pq"
+
     "github.com/go-redis/redis/v9"
 )
 
 const (
     keySize = 128
-    expiration time.Duration = 15 * time.Minute
+    authExpiration time.Duration = 15 * time.Minute
+    cacheExpiration time.Duration = time.Minute
 )
 
 type Data struct {
@@ -23,18 +26,27 @@ type Data struct {
 }
 
 type User struct {
-    nickname, email string
+    Nickname string `json: "nickname"`
+    Email string `json: "email"`
 }
 
 type Operation struct {
-    User *User
-    Method string
+    User *User `json: "user"`
+    Method string `json: "method"`
 }
 
-func (d *Data) GetOperation(key string) (Operation, error) {
-    ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second) 
-    defer cancel()
-    jsonData, err := d.cache.Get(ctx, key)
+func (d *Data) Connect(redisPort, pgsPort int) {
+    d.cache = redis.NewClient(&redisOptions{
+        Addr: fmt.Sprintf("localhost: %d", redisPort),
+        Password: "",
+        DB: 0,
+    }
+
+    pgsFile, err := os.Open(pgsParamsFile)
+}
+
+func (d *Data) GetOperation(ctx context.Context, key string) (Operation, error) {
+    jsonData, err := d.cache.Get(ctx, key).Result()
     if err != nil {
         if err == redis.Nil {
             return &Operation{}, nil
@@ -43,27 +55,101 @@ func (d *Data) GetOperation(key string) (Operation, error) {
         }
     }
     var opn Operation
-    err = json.NewDecoder(strings.NewReader(jsonData)).Decode(&opn)
+    err = json.NewDecoder(strings.NewReader(jsonData.String())).Decode(&opn)
     if err != nil {
         return nil, err
     }
     return &opn, nil
 }
 
-func (d *Data) SetOperation(user User, method string) error {
+func (d *Data) SetOperation(ctx context.Context, user User, method string) (string, error) {
     opn := Operation{&user, method}
     buf := new(strings.Builder) 
     err := json.NewEncoder(buf).Encode(&opn)
     if err != nil {
-        return err
+        return "", err
     }
-    ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
-    defer cancel()
-    key := make([]byte, keySize)
+    keyBuf := make([]byte, keySize)
     _, err = rand.Read(key)
     if err != nil {
-        return err
+        return "", err
     }
-    err = d.cache.Set(ctx, base64.URLEncoding.EncodeToString(key), buf.String(), expiration).Err()
+    key := base64.URLEncoding.EncodeToString(keyBuf)
+    err = d.cache.Set(ctx, key, buf.String(), authExpiration).Err()
+    if err != nil {
+        return "", err
+    }
+    return key, nil
+}
+
+func (d *Data) CheckNicknameInDatabase(ctx context.Context, nickname string) (bool, error) {
+    result, err := d.cache.Get(ctx, nickname).Bool()
+    if err == redis.Nil {
+        rows, err := d.db.QueryContext(ctx, "SELECT nickname FROM Users WHERE nickname = ?", nickname)        
+        defer rows.Close()
+        if err != nil {
+            return false, err
+        }
+        result = rows.Next()
+        _ = d.cache.Set(ctx, nickname, result, cacheExpiration)
+    } else if err != nil {
+        return false, err
+    }
+    return result, err
+}
+
+func (d *Data) AddUserToDatabase(ctx context.Context, user User) error {
+    _, err := d.db.ExecContext(ctx, "INSERT INTO Users VALUES ($1, $2)", user.Nickname, user.Email)
     return err
 }
+
+func (d *Data) DeleteUserFromDatabase(ctx context.Context, user User) error {
+    defer cancel()
+    _, err := d.db.ExecContext(ctx, "DELETE FROM Users WHERE nickname=$1 AND email=$2", user.Nickname, user.Email)
+    return err
+}
+
+func (d *Data) GetUsersFromDatabase() ([]User, error) {
+    var usersList []User
+    jsonData, err := d.cache.Get(ctx, "UsersList") 
+    if err == redis.Nil {
+        if usersList, err = d.cacheMiss(); err != nil {
+            return nil, err
+        }
+    } else if err != nil {
+        return nil, err
+    } else if err := json.NewDecoder(strings.NewReader(jsonData)).Decode(&usersList); err != nil {
+        return nil, err
+    }
+    return usersList
+}
+
+func (d *Data) cacheMiss() ([]User, error) {
+    var usersList []User
+    var rows *sql.Rows
+    rows, err = d.db.QueryContext(ctx, "SELECT nickname, email FROM Users")
+    defer rows.Close()
+    if err != nil {
+        return nil, err
+    }
+    for rows.Next() {
+        var user User
+        if err := rows.Scan(&user.Nickname, &user.Email); err != nil {
+            return nil, err
+        }
+        usersList = append(usersList, user)
+    }
+    if err := rows.Err(); err != nil {
+        return nil, err
+    }
+    buf := new(strings.Builder)
+    if err := json.NewEncoder(buf).Encode(&usersList); err != nil {
+        return nil, err
+    }
+    if err := d.cache.Set(ctx, "UsersList", buf.String(), cacheExpiration); err != nil {
+        return nil, err
+    }
+    return usersList, nil 
+}
+    
+
--- internal/user_handling/server/server.go
+++ internal/user_handling/server/server.go
@@ -15,14 +15,14 @@ type userHandlingServer struct {
 }
 
 func (s *userHandlingServer) AuthUser(ctx context.Context, key *pb.Key) (*pb.Response, error) {
-    operation, err := s.GetOperation(key) 
+    operation, err := s.GetOperation(ctx, key) 
     if err != nil {
         return nil, err
     }
     if operation.Method == "ADD" {
-        AddToDatabase(operation.User)
+        s.AddUserToDatabase(ctx, operation.User)
     } else if operation.Method == "DELETE" {
-        DeleteFromDatabase(operation.User)
+        s.DeleteUserFromDatabase(ctx, operation.User)
     } else {
         return &pb.Response{Message: "Wrong key."}, nil
     }
@@ -30,30 +30,48 @@ func (s *userHandlingServer) AuthUser(ctx context.Context, key *pb.Key) (*pb.Res
 }
 
 func (s *userHandlingServer) AddUser(ctx context.Context, user *pb.User) (*pb.Response, error) {
-    if ok, err := CheckDatabase(user.Nickname); err != nil {
+    if ok, err := s.CheckNicknameInDatabase(ctx, user.Nickname); err != nil {
         return nil, err
     } else if ok {
         return &pb.Response{Message: "User with this nickname is already exists."}, nil
     }
-    err = s.SetOperation(user, "ADD")
+    key, err := s.SetOperation(ctx, data.User{user.Nickname, user.Email}, "ADD")
     if err != nil {
         return nil, err
     }
-    SendEmail(user.Nickname, "add")
+    SendEmail(user.Email, "add")
     return &pb.Response{Message: "OK"}, nil
 }
 
 func (s *userHandlingServer) DeleteUser(ctx context.Context, user *pb.User) (*pb.Response, error) {
-    if ok, err := CheckDatabase(user.Nickname); err != nil {
+    if ok, err := CheckNicknameInDatabase(ctx, user.Nickname); err != nil {
         return nil, err
     } else if !ok {
-        return &pb.Response{Message: "There is no user with such nickname."}
+        return &pb.Response{Message: "There is no user with such nickname."}, nil
     }
-    SendEmail(user.Nickname, "delete")
+    SendEmail(user.Email, "delete")
     return &pb.Response{Message: "OK"}, nil
 }
 
 func (s *userHandlingServer) ListUsers(stream pb.UserHandling_ListUsersServer) error {
+    dataUsersList, err := s.GetUsersFromDatabase()
+    if err != nil {
+        return err
+    }
+    pbUsersList := convertDataUsersToPBUsers(dataUsersList)
+    for _, pbUser := range pbUsersList {
+        if err := stream.Send(pbUser); err != nil {
+            return err
+        }
+    }
+    return nil
+}
 
+func convertDataUsersToPBUsers(dataUsers []data.User) []*pb.User {
+    pbUsers := make([]*pb.User, len(dataUsers))
+    for i, dataUser := range dataUsers {
+        pbUsers[i] = &pb.User{Nickname: dataUser.Nickname, Email: dataUser.Email}
+    }
+    return psUsers
 }
 
